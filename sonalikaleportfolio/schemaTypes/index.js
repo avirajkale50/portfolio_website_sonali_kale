46fcba3333343a5e5f91769d948cf27c9dbc2031
Aviraj Kale <125662965+avirajkale50@users.noreply.github.com>
2024-05-19 10:35:28 +0530

    The first and base version for the portfolio website

---
+++ sonalikaleportfolio/schemaTypes/index.js
@@ -0,0 +1,19 @@
+import abouts from "./abouts"
+import brands from "./brands"
+import experiences from "./experiences"
+import testimonials from "./testimonials"
+import workExperience from "./workExperience"
+import works from "./works"
+import skills from "./skills"
+import contact from "./contact"
+
+export const schemaTypes = [
+    testimonials,
+    brands,
+    works,
+    experiences,
+    abouts,
+    workExperience,
+    skills,
+    contact
+]
---
+++ sonalikaleportfolio_frontend/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {Header,Footer,About,Work,Testimonial,Skills} from './container';
+import {Navbar} from './components';
+import "./App.scss";
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonial/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App;
